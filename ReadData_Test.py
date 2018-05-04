6e5e23c0c06bfc62f18eaeb09fd56c15398d9f49
Greta Studier <gretastudier@yahoo.com>
2018-05-04 14:52:09 -0500

    Fixed hover tool

--- ReadData_Test.py
+++ ReadData_Test.py
@@ -9,55 +9,34 @@ df = pd.read_csv('/Users/gretastudier/PycharmProjects/PulsarDataVisualization/Da
 cols = [0,2,4,5,7,8,10,11,14]
 df.drop(df.columns[cols], axis=1, inplace=True)
 df.columns = ['Name', 'Period', 'Frequency', 'DM','Distance', 'DistanceDM']
-#print df.head()
 
-#df.drop([0,1], axis=0,inplace=True)
 for i in range(0,2635):
     if (df.ix[i][4] == '*' or df.ix[i][4] == 'NaN' or df.ix[i][1] == '*' or df.ix[i][1] == 'NaN' or df.ix[i][5] == '*' or df.ix[i][5] == 'NaN'):
         df.drop([i], axis=0, inplace=True)
-#print df.tail(40)
 
-print df.Name
+df.Distance = np.array(df.Distance)
+df.Distance = df.Distance.astype(np.float)
+df.Period = np.array(df.Period)
+df.Period = df.Period.astype(np.float)
+
+source = ColumnDataSource(df)
 
 #Plot Data
-n = np.array(df.Name)
-
-r = np.array(df.Distance)
-r = r.astype(np.float)
-
-p = np.array(df.Period)
-p = p.astype(np.float)
-
-x = r
-y = p
-N = 2572
-# N = 4000
-# x = np.random.random(size=N) * 100
-# y = np.random.random(size=N) * 100
 radii = 0.3 #np.random.random(size=N) * 1.5
 # colors = [
 #     "#%02x%02x%02x" % (int(r), int(g), 180) for r, g in zip(50+2*x, 30+2*y)   #"#d25582"
 # ]
 
-#TOOLS="hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,"
 tools = "pan,wheel_zoom,box_zoom,reset,save".split(',')
-hover = HoverTool(tooltips=[
-    ("name", "@Name"),
-    ("Distance", "@x"),
-    ("Period", "@y")
-])
-tools.append(hover)
-
-#p = figure(tools=tools, toolbar_location="above", logo="grey", plot_width=1200, title=TITLE)
-p1 = figure(tools=tools, x_axis_label='Distance (kpc)', y_axis_label='Period (s)', title="Distance vs Period for Known Pulsars") #tools=TOOLS)
+p1 = figure(x_axis_label='Distance (kpc)', y_axis_label='Period (s)', title="Period vs Distance for Known Pulsars")
 
 #"@foo{($ 0.00 a)}" # formats 1230974 as: $ 1.23 m
 
 #p1.add_tools(HoverTool( tooltips=[('Index', '$index'),('Pulsar', '@n'),("Distance (kpc)", "@x"), ("Period (s)", "@y")]))
 
 #c1 = p.circle(x=x, y=y, color='white', fill_color="#0099ff", alpha = 0.5, size=6)
-c1 = p1.circle(x=x, y=y, color='white',fill_color='#66aecc', alpha = 0.8, size=7)
-
+c1 = p1.circle('Distance', 'Period', color='white',fill_color='#66aecc', alpha = 0.8, size=7, source = source)
+hover = HoverTool(tooltips=[('Name', '@Name'),('Period', '@Period'),('Distance','@Distance')])
 #p.add_tools(HoverTool(renderers=[c1], tooltips=[("Years", "@x"),("Women", "@y"),]))
 #p.scatter(x, y, radius=radii, color = 'green', #fill_color='white', fill_alpha=0.5, line_color=None)
 
@@ -73,12 +52,13 @@ p1.ygrid.grid_line_alpha = 0.5
 #p.image_url(url=['/Users/gretastudier/Desktop/pulsar_image.jpg'], x=42, y=8.5, h=2, w=15)
 
 
-p2 = figure(x_range=(0,1), y_range=(0,1),plot_width=300, plot_height=300)
-p2.image_url(url=['/Users/gretastudier/Desktop/pulsar_image.jpg'], x=0, y=1, h=1, w=1)
+#p2 = figure(x_range=(0,1), y_range=(0,1),plot_width=300, plot_height=300)
+#p2.image_url(url=['/Users/gretastudier/Desktop/pulsar_image.jpg'], x=0, y=1, h=1, w=1)
 
+p1.add_tools(hover)
 output_file("color_scatter.html", title="color_scatter.py example")
-
-show(row(p1, p2))
+show(p1)
+#show(row(p1, p2))
 
 
 
